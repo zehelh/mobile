89515e4385883b61dc10cf175b82d486f0c3476d
MaxenceBannier <maxence_86@hotmail.fr>
2021-04-23 14:47:02 +0200

    add onclick

--- app/src/main/java/com/example/forms/MainActivity.kt
+++ app/src/main/java/com/example/forms/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.forms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.inflate
 
 class MainActivity : AppCompatActivity() {
+    //override fun onCreate(savedInstanceState: Bundle?) {
+    //   super.onCreate(savedInstanceState)
+    //   setContentView(R.layout.activity_main)
+    //}
+
+    private lateinit var binding: ResultProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ResultProfileBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
+
+class ResultProfileBinding {
+
+}
+
