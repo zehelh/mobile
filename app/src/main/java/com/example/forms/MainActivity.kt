9dcd235fddddccb50692b90c6225b87d5472a9f6
MaxenceBannier <maxence_86@hotmail.fr>
2021-04-30 16:01:11 +0200

    commit check oui yvan c a toi

--- app/src/main/java/com/example/forms/MainActivity.kt
+++ app/src/main/java/com/example/forms/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.forms
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
 import com.example.forms.databinding.ActivityMainBinding
 
 
@@ -17,10 +19,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.submit.setOnClickListener {
-            val activity2 = Intent(this, MainActivity2::class.java)
-            activity2.putExtra("firstname", binding.firstnameInput.text)
-            activity2.putExtra("name", binding.nameInput.text)
-            startActivity(activity2)
+            if (binding.nameInput.textfield.isEmpty() || binding.firstnameInput.text.isEmpty()) {
+                val myToast = Toast.makeText(applicationContext, "Ce champ est vide", Toast.LENGTH_SHORT)
+                myToast.setGravity(Gravity.LEFT, 200, 200)
+                myToast.show()
+            } else {
+                val activity2 = Intent(this, MainActivity2::class.java)
+                activity2.putExtra("firstname", binding.firstnameInput.text)
+                activity2.putExtra("name", binding.nameInput.text)
+                startActivity(activity2)
+            }
+
         }
 
     }
